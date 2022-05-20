d176cfe9ddfb75969824510a1651af706ab00587
IT21062360 <99788522+IT21062360@users.noreply.github.com>
2022-05-20 22:54:48 +0530

    IT21062360

---
+++ report (1).h
@@ -0,0 +1,11 @@
+class Report{
+
+Report()
+Report(int rNo)
+void genarateCourseReport(char array[])
+void genarateWebsiteReport(char array[])
+void displayCoursesReport()
+void displayWebsiteReport()
+~Report()
+
+}
